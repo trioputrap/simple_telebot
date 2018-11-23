656a8f6eb8409288c9d48996a2a546b54fe8b670
trioputrap <trioputrap@gmail.com>
2018-11-23 14:13:49 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "newpass";
+    $db = "itccbot_db";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
---
+++ listener.php
@@ -0,0 +1,32 @@
+<?php
+    define('BOT_TOKEN', '761669654:AAEflIkOxaOTeRlaUZdSnmXqzYdEI-NTSfA');
+    define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+    	
+    // read incoming info and grab the chatID
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chatID = $update["message"]["chat"]["id"];
+    		
+    // compose reply
+    $reply =  sendMessage();
+    		
+    // send reply
+    $sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+    file_get_contents($sendto);
+    
+    function sendMessage(){
+        $message = "I am a baby bot.";
+        return $message;
+    }
+    
+    checkJSON($chatID,$update);
+
+	function checkJSON($chatID,$update){
+	
+		$myFile = "log.txt";
+		$updateArray = print_r($update,TRUE);
+		$fh = fopen($myFile, 'a') or die("can't open file");
+		fwrite($fh, $chatID ."\n\n");
+		fwrite($fh, $updateArray."\n\n");
+		fclose($fh);
+	}
