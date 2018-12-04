991547756385199a199c4451b51371b19337f718
trioputrap <trioputrap@gmail.com>
2018-12-04 21:53:12 +0800

    final

--- processor.php
+++ processor.php
@@ -6,58 +6,63 @@
 
         if($qry->num_rows > 0) {
             while($data = $qry->fetch_object()){
-                $messages = explode(" #", $data->message);
-                
-                $reply = "";
-
-                $sql2 = "SELECT `sql` FROM operation WHERE keyword='".$messages[0]."'";
-                
+                $sql2 = "SELECT `format` FROM operation WHERE id='".$data->message."'";
                 $qry2 = $conn->query($sql2);
-                
                 if($qry2->num_rows > 0) {
                     $data2 = $qry2->fetch_object();
-
-                    $sql2 = $data2->sql;
-
-                    foreach($messages as $key => $val) {
-                        if($key==0) continue;
-                        $sql2 = substr_replace($sql2, $val, strpos($sql2, "?"), 1);
-                    }
-
-                    echo $sql2;
-
-                    $qry2 = $conn->query($sql2);
-                    $num_rows = $qry2->num_rows;
-                    if($num_rows > 0) {
-                        while($data2 = $qry2->fetch_assoc()){            
-                            foreach($data2 as $key => $val){
-                                $reply.=$key."\t: ".$val."\n";
-                            }
-                            if($num_rows>1) $reply.="\n";
-                        }
-                        echo $reply;
-                    } else {
-                        $reply = "Data tidak ditemukan";
-                    }
+                    $reply = $data2->format;
                 } else {
-                    $sql2 = "SELECT name, format FROM operation";
+                    $messages = explode(" #", $data->message);
+                
+                    $reply = "";
+
+                    $sql2 = "SELECT `sql` FROM operation WHERE keyword='".$messages[0]."'";
+                    
                     $qry2 = $conn->query($sql2);
                     
-                    $i = 0;
-                    while($data2 = $qry2->fetch_object()) {
-                        $i++;
-                        $reply .= $i.".\t".$data2->name."\n";
-                        $reply .= "\t".$data2->format."\n\n";
+                    if($qry2->num_rows > 0) {
+                        $data2 = $qry2->fetch_object();
+
+                        $sql2 = $data2->sql;
+
+                        foreach($messages as $key => $val) {
+                            if($key==0) continue;
+                            $sql2 = substr_replace($sql2, $val, strpos($sql2, "?"), 1);
+                        }
+
+
+                        $qry2 = $conn->query($sql2);
+                        $num_rows = $qry2->num_rows;
+                        if($num_rows > 0) {
+                            while($data2 = $qry2->fetch_assoc()){            
+                                foreach($data2 as $key => $val){
+                                    $reply.=$key."\t: ".$val."\n";
+                                }
+                                if($num_rows>1) $reply.="\n";
+                            }
+                        } else {
+                            $reply = "Data tidak ditemukan";
+                        }
+                    } else {
+                        $sql2 = "SELECT name  FROM operation";
+                        $qry2 = $conn->query($sql2);
+                        
+                        $i = 0;
+                        while($data2 = $qry2->fetch_object()) {
+                            $i++;
+                            $reply .= $i.".\t".$data2->name."\n";
+                        }
                     }
                 }
-                
+
+
                 $sql2 = "INSERT INTO outbox (
                             message, 
                             chat_id, 
                             date_insert, 
                             flag)
                         VALUES (
-                            '".urlencode("msg: ".$data->message."\nreply: ".$reply)."', 
+                            '".urlencode($reply)."', 
                             '".$data->chat_id."', 
                             '".date('Y-m-d H:i:s')."', 
                             0)";
@@ -66,6 +71,7 @@
                 //update flag inbox true
                 $sql2 = "UPDATE inbox SET flag=1 WHERE id = ".$data->id;
                 $qry2 = $conn->query($sql2);
+                echo $reply;
             }
         } else {
             echo "no new message to process...\n";
